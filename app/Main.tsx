e16b926949c6b29c7d15b8d5b9ada1f986087963
JIeJaitt <498938874@qq.com>
2024-11-13 20:42:01 +0800

    feat: study nextjs in 2024-11-13 20:41

---
+++ app/Main.tsx
@@ -0,0 +1,14 @@
+import Link from '@/components/Link'
+import Tag from '@/components/Tag'
+
+const MAX_DISPLAY = 5
+
+export default function Home({ posts }) {
+    return (
+        <>
+        <div className="divide-y divide-gray-200 dark:divide-gray-700"></div>
+        <div className="space-y-2"></div>
+        </>
+        
+    )
+}
